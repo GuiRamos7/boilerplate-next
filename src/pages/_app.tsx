a6c7f7bccd86677405f728d401a6cfdfcbc7f249
Guilherme Ramos <guilherme.ramos@matchboxbrasil.com>
2021-04-12 05:35:39 -0300

    Add next pwa

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
-import { AppProps } from "next/dist/next-server/lib/router/router";
-import Head from "next/head";
-import GlobalStyled from "styles/global";
+import { AppProps } from 'next/dist/next-server/lib/router/router'
+import Head from 'next/head'
+import GlobalStyled from 'styles/global'
 
 function App({ Component, pageProps }: AppProps) {
   return (
@@ -9,6 +9,8 @@ function App({ Component, pageProps }: AppProps) {
         <title>React Avançado - Borlerplate</title>
         <link rel="shortchut icon" href="/img/icon-512.png" />
         <link rel="shortchut icon" href="/img/icon-192.png" />
+        <link rel="manifest" href="/manifest.json" />
+
         <meta
           name="description"
           content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
@@ -17,7 +19,7 @@ function App({ Component, pageProps }: AppProps) {
       <GlobalStyled />
       <Component {...pageProps} />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
